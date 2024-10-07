f9c0b944b99f97ef53a2e090b041a2e41d560b57
hassanAbdullahiftikhar <157296548+hassanAbdullahiftikhar@users.noreply.github.com>
2024-10-07 23:48:41 +0500

    Update data_struct.h added stack to record palyer  positions

--- data_struct.h
+++ data_struct.h
@@ -1,2 +1,46 @@
 #pragma once
 
+struct location {
+	int x, y;
+	location* next;
+	location(int c1, int c2) :x(c1), y(c2) {
+		next = nullptr;
+	}
+};
+class stack {
+	location* top;
+public:
+	stack() {
+		top = nullptr;
+	}
+	void insert(int x, int y) {
+		if (top == nullptr) {
+			top = new location(x, y);
+		}
+		else {
+			location* curr = new location(x, y);
+			curr->next = top;
+			top = curr;
+		}
+	}
+	bool is_empty() {
+		return top == nullptr;
+	}
+	location*& t() {
+		return top;
+	}
+	void pop() {
+		if (top != nullptr) {
+			if (top->next == nullptr) {
+				location* curr = top;
+				delete curr;
+				top = nullptr;
+			}
+			else {
+				location* curr = top;
+				top = top->next;
+				delete curr;
+			}
+		}
+	}
+};
