212c4edd02986e43dfe898df6ab3b52c19f2ac94
hassanAbdullahiftikhar <157296548+hassanAbdullahiftikhar@users.noreply.github.com>
2024-10-15 14:55:13 +0500

    Update data_struct.h to include coin queue to keep track of coins collected

--- data_struct.h
+++ data_struct.h
@@ -44,3 +44,39 @@ public:
 		}
 	}
 };
+class coin_queue {
+	location* first=nullptr;
+	location* tail = nullptr;
+public:
+	int nn = 0;
+	coin_queue() {
+		first = nullptr;
+		tail = nullptr;
+	}
+	void enque(int x,int y) {
+		if (first == nullptr) {
+			first = new location(x, y);
+			tail = first;
+			first->next = nullptr;
+		}
+		else {
+			location* cu = tail;
+			location* n = new location(x, y);
+			cu->next = n;
+			tail = n;
+		}
+		nn++;
+	}
+	void print(int r) {
+		location* cu = first;
+		mvprintw(r+3,0,"Coins collected:");
+		int row = r+4;
+		for (int i = 0; i < nn; i++) {
+			mvprintw(row+i, 0, "(");
+			mvprintw(row+i, 1, "%d", cu->y);
+			mvprintw(row+i, 3, "%d", cu->x);
+			mvprintw(row+i, 5, ")");
+			cu = cu->next;
+		}
+	}
+};
